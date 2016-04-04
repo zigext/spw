919da5b818055595cf7a138261b1ae867fbec357
zigext <bynx1234>
2016-04-04 19:32:19 +0700

    #01 Frame

---
+++ spw/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class Main {
+	public static void main(String[] args){
+		JFrame frame = new JFrame("Space War");
+		frame.setSize(400,650);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+
+
+		}
+	}
